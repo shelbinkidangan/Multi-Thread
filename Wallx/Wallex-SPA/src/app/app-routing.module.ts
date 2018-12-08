f56a0f7a969ac0d9a17b4d82a26c1582bd9a4555
shelbincormsquare <shelbin@cormsquare.com>
2018-12-08 19:14:57 +0530

    Added WAllex

--- Wallx/Wallex-SPA/src/app/app-routing.module.ts
+++ Wallx/Wallex-SPA/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PublicLayoutComponent } from './public-layout/public-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  // Main redirect
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: '', component: PublicLayoutComponent,
+    children: [
+      { path: 'home', component: HomeComponent },
+    ]
+  },
+
+  // Handle all other routes
+  { path: '**', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Wallx/Wallex-SPA/src/app/app.module.ts
+++ Wallx/Wallex-SPA/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PublicLayoutComponent } from './public-layout/public-layout.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PublicLayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
